8c206821c5912cc9ed04e65507246515cfe4af94
LaBatata101 <labatata101@linuxmail.org>
2022-10-27 18:53:22 -0300

    add Spring main file

---
+++ src/main/java/com/ifce/academico/AcademicoIfceApplication.java
@@ -0,0 +1,13 @@
+package com.ifce.academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicoIfceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicoIfceApplication.class, args);
+    }
+
+}
