3c84a9134b3f86d69a0dd3376e4db64728a508e0
LaBatata101 <labatata101@linuxmail.org>
2022-10-27 18:48:01 -0300

    add test folder

---
+++ src/test/java/com/ifce/academico/AcademicoIfceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifce.academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicoIfceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
