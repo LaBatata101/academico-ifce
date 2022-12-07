955854104cf9c015e8085016531163c63f86bd9b
LaBatata101 <labatata101@linuxmail.org>
2022-12-06 22:54:05 -0300

    rename StudentNotFoundHandler class to a more generic name
    This way we don't need to create a new NotFoundHandler class for every entity.

--- src/main/java/com/ifce/academico/handler/NotFoundHandler.java
+++ src/main/java/com/ifce/academico/handler/NotFoundHandler.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class StudentNotFoundHandler {
+public class NotFoundHandler {
 
     @ResponseBody
     @ExceptionHandler(StudentNotFoundException.class)
