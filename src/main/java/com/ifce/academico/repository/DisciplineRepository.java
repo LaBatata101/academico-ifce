0af860bff24a9104f0ab73e69a6f62e7d19250ba
LaBatata101 <labatata101@linuxmail.org>
2022-12-20 17:38:10 -0300

    add repository for Discipline

--- src/main/java/com/ifce/academico/repository/DisciplineRepository.java
+++ src/main/java/com/ifce/academico/repository/DisciplineRepository.java
@@ -2,6 +2,17 @@ package com.ifce.academico.repository;
 
 import com.ifce.academico.model.Discipline;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface DisciplineRepository extends JpaRepository<Discipline, Long> {
+
+    @Query("SELECT d FROM Discipline as d WHERE d.name = :name")
+    Optional<Discipline> findByName(@Param("name") String name);
+
+    @Query("SELECT d FROM Discipline as d WHERE d.name IN :names")
+    List<Discipline> findAllByNames(@Param("names") List<String> names);
 }
