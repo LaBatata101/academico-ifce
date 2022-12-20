8dbd81c032a3ab1b6979e4f7ceaad8474972be84
LaBatata101 <labatata101@linuxmail.org>
2022-12-20 17:36:03 -0300

    remove field validation from Professor DB model

--- src/main/java/com/ifce/academico/model/Professor.java
+++ src/main/java/com/ifce/academico/model/Professor.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -18,17 +16,10 @@ public class Professor {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotBlank(message = "name is mandatory")
     private String name;
-
-    @Email
-    @NotBlank(message = "email is mandatory")
     private String email;
-
-    @Pattern(regexp = "\\([0-9]{2}\\)[0-9]{9}|[0-9]{9}", message = "Invalid phone number")
-    @NotBlank(message = "phone number is mandatory")
     private String phone;
 
     @ManyToMany(mappedBy = "professors")
-    private List<Discipline> disciplines;
+    private List<Discipline> disciplines = new ArrayList<>();
 }
