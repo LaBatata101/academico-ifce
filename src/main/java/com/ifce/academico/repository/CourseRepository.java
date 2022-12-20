3edd11186b501da85a1418a95edeb852ec46fcb0
LaBatata101 <labatata101@linuxmail.org>
2022-12-20 17:38:40 -0300

    add function to find courses by name

--- src/main/java/com/ifce/academico/repository/CourseRepository.java
+++ src/main/java/com/ifce/academico/repository/CourseRepository.java
@@ -2,6 +2,12 @@ package com.ifce.academico.repository;
 
 import com.ifce.academico.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    @Query("SELECT d FROM Course as d WHERE d.name = :name")
+    Optional<Course> findByName(@Param("name") String name);
 }
