03e6ab0f06281840d1ed341f0c19412e9e1cc82e
LaBatata101 <labatata101@linuxmail.org>
2022-12-20 17:40:23 -0300

    add constructor with a name parameter

--- src/main/java/com/ifce/academico/exception/CourseNotFoundException.java
+++ src/main/java/com/ifce/academico/exception/CourseNotFoundException.java
@@ -4,4 +4,8 @@ public class CourseNotFoundException extends RuntimeException {
     public CourseNotFoundException(Long id) {
         super("Could not find course with id: " + id);
     }
+
+    public CourseNotFoundException(String name) {
+        super("Could not find course: " + name);
+    }
 }
